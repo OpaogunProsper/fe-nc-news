f15beeafb3a3cfec3556da46f8c9089e2e3baccb
OpaogunProsper <prblvck@gmail.com>
2024-08-06 12:14:46 +0100

    List all articles

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>NC news</h1>
+        <button>Account</button>
+      </header>
+    </>
+  );
+}
+export default Header;
